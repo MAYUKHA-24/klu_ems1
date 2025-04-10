a8b4b56dd27e969a932cd0c496977487b4aaa423
MAYUKHA-24 <bbslmayukha24306@gmail.com>
2025-04-10 12:42:10 +0530

    second commit

--- src/main/java/com/klu/JwtDemo1Application.java
+++ src/main/java/com/klu/JwtDemo1Application.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class JwtDemo1Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(JwtDemo1Application.class, args);
+		SpringApplication.run(JwtDemo1Application.class, args); //it is a main method.
 	}
 
 }
